1d6ef6bde74f3dc4a1e4d819b9d1b5bed3732c9a
stupidMF <rafal.latoszek97@gmail.com>
2020-03-17 18:16:19 +0100

    create spawner and opponent

---
+++ Assets/Script/Level/Opponent.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Opponent
+{
+    GameObject GameObject { get; set; }
+
+    int Id { get; set; }
+
+    int LifePoints { get; set; } = 100;
+
+    int KillCount { get; set; } = 0;
+
+    int Points { get; set; } = 0;
+
+    float VisionResolution { get; set; }
+
+    float VisionAngle { get; set; }
+
+    int[] VisionTable { get; set; }
+
+    public Opponent(float visionAngle, float visionResolution)
+    {
+        int tableSize = ((int)((visionAngle / 2) / visionResolution)) * 2 + 1;
+        VisionResolution = visionResolution;
+        VisionAngle = visionAngle;
+        VisionTable = new int[tableSize];
+    }
+}
---
+++ Assets/Script/Level/Spawner.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+class Spawner
+{
+    public List<Vector3> SpawnPoints { get; set; }
+
+    int numberSpawnPoints = 4;
+
+    public Spawner(
+        Vector2 gridSize, int minBorderMargin, int minMarginBetween, List<Obstacle> obstacles)
+    {
+        SpawnPoints = new List<Vector3>();
+        for (int i = 0; i < numberSpawnPoints; i++)
+        {
+            SpawnPoints.Add(CreateClearPosition(gridSize, minBorderMargin, minMarginBetween, obstacles));
+        }
+    }
+
+    Vector3 CreateClearPosition(
+        Vector2 gridSize, int minBorderMargin, int minMarginBetween, List<Obstacle> obstacles)
+    {
+        Vector3 randPosition;
+        bool isClear;
+
+        do
+        {
+            isClear = true;
+
+            randPosition = new Vector3(
+                (int)Random.Range(0 + minBorderMargin, gridSize.x - minBorderMargin),
+                0,
+                (int)Random.Range(0 + minBorderMargin, gridSize.y - minBorderMargin)
+            );
+
+            for (int i = 0; i < obstacles.Count; i++)
+            {
+                if (DistanceBetween(randPosition.x, obstacles[i].Position.x, obstacles[i].Scale.x) < minMarginBetween
+                    || DistanceBetween(randPosition.y, obstacles[i].Position.y, obstacles[i].Scale.y) < minMarginBetween
+                )
+                {
+                    isClear = false;
+                }
+            }
+        } while (!isClear);
+
+        return randPosition;
+    }
+
+    float DistanceBetween(float pointPos, float obstaclePos, float obstacleSize)
+    {
+        return Mathf.Abs(pointPos - obstaclePos) - obstacleSize / 2;
+    }
+}
