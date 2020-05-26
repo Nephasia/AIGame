c94313796e05b60ef8c0fe3b700743eefe723192
Nephasia <shiverz@o2.pl>
2020-05-26 19:46:07 +0200

    ...

---
+++ Assets/Script/Temp/Receiver.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Receiver 
+{
+    public int Life { get; set; } = 100;
+
+    // Update is called once per frame
+    public void Update()
+    {
+        Debug.Log(Life);
+        if (Life < 0) Debug.Log("Died");
+
+    }
+}
---
+++ Assets/Script/Temp/ShootTemp.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootTemp 
+{
+
+    public void Update(GameObject gameObject, Receiver receiver)
+    {
+        CheckCollision(gameObject, receiver);
+        Debug.Log("runs");
+    }
+
+    private void CheckCollision(GameObject gameObject, Receiver receiver) {
+        RaycastHit hit;
+
+        float length = 30f;
+
+        if (Physics.Raycast(
+                gameObject.transform.position, 
+                gameObject.transform.TransformDirection(Vector3.forward),
+                out hit,    
+                length
+            )
+        ) {
+            Debug.Log("hit");
+            receiver.Life -= 25;
+        }
+    }
+
+}
