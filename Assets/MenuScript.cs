03b043d98e630b450879f59883e5389555775d8b
nephasia <shiverz@o2.pl>
2020-06-10 20:10:52 +0200

    ...

--- Assets/MenuScript.cs
+++ Assets/MenuScript.cs
@@ -15,3 +15,42 @@ public class MenuScript : MonoBehaviour
         Application.Quit();
     }
 }
+
+//public class MainMenu : MonoBehaviour {
+
+//	Button[] buttonMainMenu = new Button[4];
+
+//	// Use this for initialization
+//	void Start () {
+		
+//		buttonMainMenu[0] = transform.Find ("Play").GetComponent<Button> ();
+//		buttonMainMenu[0].onClick.AddListener ( delegate() { 
+//			Gameplay.Instance.Sounds.PlayForwardButton();
+//			gameObject.SetActive(false);
+//			PlayerMovement.Instance.InitPlayerMovement();
+//		});
+
+//		buttonMainMenu[1] = transform.Find ("Exit").GetComponent<Button> ();
+//		buttonMainMenu[1].onClick.AddListener ( delegate(){ 
+//			Gameplay.Instance.Sounds.PlayBackwardButton();
+//			Application.Quit();
+//		});
+
+//		buttonMainMenu[2] = transform.Find ("Settings").GetComponent<Button> ();
+//		buttonMainMenu[2].onClick.AddListener ( delegate() { 
+//			SceneManager.LoadScene(1);
+//			Gameplay.Instance.Sounds.PlayForwardButton();
+//			gameObject.SetActive(false);
+//			Gameplay.Instance.MenuCanvases.transform.Find("SettingsCanvas").gameObject.SetActive(true);
+//		});
+
+//		buttonMainMenu[3] = transform.Find ("Tutorial").GetComponent<Button> ();
+//		buttonMainMenu[3].onClick.AddListener ( delegate() { 
+//			SaveManager.Instance.data.IsTutorialTurnedOff = !SaveManager.Instance.data.IsTutorialTurnedOff;
+//			ReloadTutorialText();
+//			Gameplay.Instance.Sounds.PlayForwardButton();
+//		});
+
+//	}
+
+//}
